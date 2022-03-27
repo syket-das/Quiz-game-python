9dede538d16697bf75af278b2d77baaf8f052df1
syket-das <saiketsd23@gmail.com>
2022-03-27 09:33:07 +0530

    que and answers

---
+++ main.py
@@ -0,0 +1,35 @@
+# ------------------------------------
+def new_game():
+    pass
+# ------------------------------------
+
+
+def check_answers():
+    pass
+# ------------------------------------
+
+
+def display_score():
+    pass
+# ------------------------------------
+
+
+def play_again():
+    pass
+# ------------------------------------
+
+
+questions = {
+    "What is the capital of France?": "Paris",
+    "What is the capital of Spain?": "Madrid",
+    "What is the capital of Italy?": "Rome",
+    "What is the capital of Germany?": "Berlin",
+}
+
+
+options = [
+    ["A. Paris", "B. Madrid", "C. Rome", "D. Berlin"],
+    ["A. Rome", "B. Berlin", "C. Paris", "D. Madrid"],
+    ["A. Berlin", "B. Rome", "C. Madrid", "D. Paris"],
+    ["A. Madrid", "B. Berlin", "C. Rome", "D. Paris"]
+]
